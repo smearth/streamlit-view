f71bb59495d180a9508ca0eba40e91d81989e8f6
smearth <102247923+smearth@users.noreply.github.com>
2022-10-17 03:00:44 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+view = [100,150,30]
+st.write('# Youtube')
+st.write('## Youtube')
+view
+st.write('## bar')
+st.bar_chart(view)
+import pandas as pd
+sview = pd.Series(view)
+sview
